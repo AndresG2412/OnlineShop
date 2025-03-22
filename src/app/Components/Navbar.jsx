09a621fd6e6270f14a2d092d2de3422622fb5974
Andres Gaviria <cgaviria930@gmail.com>
2025-03-22 09:10:19 -0500

    nuevos avances navabr y extras

---
+++ src/app/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='h-16 w-screen bg-white shadow-2xl rounded-xl fixed top-2'>
+      
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
     <>
       <p>
+        <Navbar />
         Home Page
       </p>
     </>
