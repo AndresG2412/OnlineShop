64bb6903381bc1465bc0581e9e46f7e89227e72c
Andres Gaviria <cgaviria930@gmail.com>
2025-03-24 11:07:20 -0500

    redirecciones pagina

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <div className='h-16 w-screen bg-gray-200 shadow-2xl fixed border-b-[#222]'>
-      
+      <Link href="/">Productos</Link>
+      <Link href="../Pages/Promociones">Promociones</Link>
+      <Link href="../Pages/Carrito">Carrito</Link>
     </div>
   )
 }
