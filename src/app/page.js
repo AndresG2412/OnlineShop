fab641db9b5f11df5379ed78add4edd8ccec56a0
Andres Gaviria <cgaviria930@gmail.com>
2025-03-24 11:09:49 -0500

    ajustes navbar import

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <p>
         Home Page
       </p>
