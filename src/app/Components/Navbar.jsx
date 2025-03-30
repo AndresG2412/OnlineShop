4c06316cbd1eff1d1e66866781fc6b4c98744d5f
Andres Gaviria <cgaviria930@gmail.com>
2025-03-29 20:16:27 -0500

    cambios navbar

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -7,13 +7,13 @@ import logo from '../Images/Logo2.webp'
 
 export default function Navbar() {
   return (
-    <div className='h-16 w-screen bg-transparent shadow-2xl fixed flex justify-center items-center border-b-4 border-[#222]'>
+    <div className='h-16 w-screen bg-transparent shadow-1 fixed flex justify-center items-center border-b-[1px] border-[#fff]'>
       <Image src={logo} className='hidden h-12 w-auto mx-4 hover:scale-110 hover:transition-all' alt="logo_main"/>
 
-      <div className='flex gap-10 mr-12'>
-        <Link className='uppercase text-white font-bold text-xl tracking-widest hover:duration-300 hover:scale-110 hover:transition-all' href="/">Productos</Link>
+      <div className='flex gap-20 mr-12'>
+        <Link className='uppercase text-white font-bold text-2xl tracking-widest hover:duration-300 hover:scale-110 hover:transition-all' href="/">Productos</Link>
         {/* <Link className='uppercase text-white font-bold text-xl tracking-widest hover:duration-300 hover:scale-110 hover:transition-all' href="../Pages/Promociones">Promociones</Link> */}
-        <Link className='uppercase text-white font-bold text-xl tracking-widest hover:duration-300 hover:scale-110 hover:transition-all' href="../Pages/Carrito">Carrito</Link>
+        <Link className='uppercase text-white font-bold text-2xl tracking-widest hover:duration-300 hover:scale-110 hover:transition-all' href="../Pages/Carrito">Carrito</Link>
       </div>
 
       <Image src={user} className='hidden h-10 w-10 mx-4 hover:scale-110 hover:transition-all' alt='user_logo'/>
