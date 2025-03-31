e35a5b9bbc0d3063e5b8c00722ca7b3ca2f60e1b
Andres Gaviria <cgaviria930@gmail.com>
2025-03-31 18:21:21 -0500

    avances login admin

--- src/app/Pages/Admin/page.jsx
+++ src/app/Pages/Admin/page.jsx
@@ -1,44 +1,47 @@
 import React from 'react'
 
+import logo from '../../Images/Logo2.webp'
+import Image from 'next/image'
+
 export default function Login() {
   return (
-    /* From Uiverse.io by Ameth1208 */ 
-    <div class="relative py-3 sm:max-w-xs sm:mx-auto">
-        <div
-            class="min-h-96 px-8 py-6 mt-4 text-left bg-white dark:bg-gray-900 rounded-xl shadow-lg"
-        >
-            <div class="flex flex-col justify-center items-center h-full select-none">
-                <div class="flex flex-col items-center justify-center gap-2 mb-8">
-                    <div class="w-8 h-8 bg-gray-700" src=""></div>
-                    <p class="m-0 text-[16px] font-semibold dark:text-white">
-                    Login to your Account
-                    </p>
-                    <span class="m-0 text-xs max-w-[90%] text-center text-[#8B8E98]"
-                    >Get started with our app, just start section and enjoy experience.
-                    </span>
-                </div>
-                    <div class="w-full flex flex-col gap-2">
-                        <label class="font-semibold text-xs text-gray-400">Username</label>
-                        <input
-                        placeholder="Username"
-                        class="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900"
-                        />
+    <div className='h-screen flex items-center'>
+        <div className="mx-auto w-1/2">
+            <div
+                className="min-h-96 px-8 py-6 mt-4 text-left bg-gray-900 rounded-xl shadow-lg"
+            >
+                <div className="flex flex-col justify-center items-center h-full select-none">
+                    <div className="flex flex-col items-center justify-center gap-2 mb-8">
+                        <Image 
+                            width={120}
+                            height={120}
+                            src={logo} className='mx-4 hover:scale-110 hover:transition-all' alt='user_logo'/>
+                            <p className="m-0 text-3xl font-semibold dark:text-white">
+                            Bienvenido de vuelta
+                            </p>
                     </div>
-            </div>
-            <div class="w-full flex flex-col gap-2">
-                <label class="font-semibold text-xs text-gray-400">Password</label>
-                <input
-                    placeholder="••••••••"
-                    class="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900"
-                    type="password"
-                />
-            </div>
-            <div>
-                <button
-                    class="py-1 px-8 bg-blue-500 hover:bg-blue-800 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg cursor-pointer select-none"
-                >
-                    Login
-                </button>
+                        <div className="w-full flex flex-col gap-2">
+                            <label className="font-semibold text-xl text-gray-400">Correo Electronico</label>
+                            <input 
+                                type="email" 
+                                className='w-full h-8 bg-white rounded-lg mb-4 pl-2 tracking-wider'
+                                placeholder='Ingresa tu Correo Electronico'/>
+                        </div>
+                </div>
+                    <div className="w-full flex flex-col gap-2">
+                        <label className="font-semibold text-xl text-gray-400">Contraseña</label>
+                        <input 
+                                type="password" 
+                                className='w-full h-8 bg-white rounded-lg mb-6 pl-2 tracking-wider'
+                                placeholder='Ingresa tu Contraseña'/>
+                    </div>
+                <div>
+                    <button
+                        className="tracking-wider py-1 px-8 bg-blue-500 hover:bg-blue-800 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg cursor-pointer select-none text-xl"
+                    >
+                        Ingresar
+                    </button>
+                </div>
             </div>
         </div>
     </div>
