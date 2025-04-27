c23a6738acf81fcb95d7b6c3e5e50fccaa9bdfc7
Andres Gaviria <cgaviria930@gmail.com>
2025-04-26 21:01:56 -0500

    volver a empezar estructura base de datos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,10 @@
 "use client";
 
 import Busqueda from "./Components/Specifics/Busqueda";
-import ProductsView from "./Components/ProductsView";
 
-export default function Home() {
-
+export default function Main() {
   return (
-    <div className="mt-24 w-screen h-auto">
+    <div className="flex flex-col gap-4 items-center">
       <Busqueda />
     </div>
   );
