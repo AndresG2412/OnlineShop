95159872dee9b37695e90c8f2c27bfe6e35fbeae
Andres Gaviria <cgaviria930@gmail.com>
2025-05-12 18:21:33 -0500

    cambios diseño

--- src/app/Components/Sections.jsx
+++ src/app/Components/Sections.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 
 export default function Section({nombre}) {
   return (
-    <div>
-        <div className='w-screen flex items-center justify-center px-12'>
-            <div className='border-2 border-white w-full'></div>
-            <p className='mx-4 tracking-widest font-semibold text-2xl text-white'>{nombre}</p>
-            <div className='border-2 border-white w-full'></div>
-        </div>
-    </div>
+      <div className='w-full flex items-center justify-center px-12 mb-6'>
+          <div className='border-2 border-white w-full'></div>
+          <p className='mx-4 tracking-widest font-bold uppercase text-4xl text-white'>{nombre}</p>
+          <div className='border-2 border-white w-full'></div>
+      </div>
   )
 }
