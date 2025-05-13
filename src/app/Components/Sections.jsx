427aa6821b6d79da297eebd667a79a85180ce002
Andres Gaviria <cgaviria930@gmail.com>
2025-05-12 22:19:42 -0500

    new provisional card

--- src/app/Components/Sections.jsx
+++ src/app/Components/Sections.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Section({nombre}) {
   return (
-      <div className='w-full flex items-center justify-center px-12 mb-6'>
+      <div className='w-full flex items-center justify-center mb-6'>
           <div className='border-2 border-white w-full'></div>
           <p className='mx-4 tracking-widest font-bold uppercase text-4xl text-white'>{nombre}</p>
           <div className='border-2 border-white w-full'></div>
