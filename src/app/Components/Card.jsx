96447d5491746e0d12bab566eccc69a2b3d09fa0
Andres Gaviria <cgaviria930@gmail.com>
2025-05-14 06:37:45 -0500

    design card

--- src/app/Components/Card.jsx
+++ src/app/Components/Card.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function Card({titulo, precio}) {
     return (
-        <div className='animate-zoom-in flex relative flex-col gap-y-4 justify-center bg-white rounded-lg shadow-lg p-4 pb-10 w-full'>
-            <div className='h-32 w-32 bg-red-200 rounded-xl shadow-2xl mx-auto'></div>
-            <p className='uppercase tracking-wide font-semibold text-2xl'>{titulo}</p>
-            <p className='text-center tracking-widest absolute bottom-2 font-semibold text-xl'>${precio}</p>
+        <div className='group h-full'>
+            <div className='group-hover:scale-110 group-hover:duration-300 animate-zoom-in flex relative flex-col gap-y-4 justify-center h-full bg-white rounded-lg shadow-lg p-4 pb-10 w-full'>
+                <div className='h-32 w-32 bg-red-200 rounded-xl shadow-2xl mx-auto'></div>
+                <p className='uppercase tracking-wide font-semibold text-xl'>{titulo}</p>
+                <p className='text-center tracking-widest absolute bottom-2 font-semibold text-xl'>${precio}</p>
+            </div>
         </div>
   )
 }
