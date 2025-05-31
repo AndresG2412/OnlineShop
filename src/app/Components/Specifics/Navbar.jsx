f76c457c5e3b7008ddb92812613ddc595ea408ce
Andres Gaviria <cgaviria930@gmail.com>
2025-05-31 13:14:42 -0500

    logo a navbar

--- src/app/Components/Specifics/Navbar.jsx
+++ src/app/Components/Specifics/Navbar.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import logo from '../../images/CARO-SHOP1.webp'
 
 import { ROUTES } from '@/routes';
 
@@ -31,25 +32,12 @@ export default function Navbar() {
           />
         </Link>
 
-        {/* Menú desktop - oculto en móvil
-        <div className='hidden md:flex mr-8 gap-x-8'>
-          <div className='flex items-center gap-x-2 hover:duration-300 hover:scale-110 hover:transition-all'>
-            <Link className='uppercase text-white font-bold text-2xl tracking-widest' href={ROUTES.HOME}>
-              Productos
-            </Link>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="invert transform scale-125 size-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
-            </svg>
-          </div>
-          <div className='flex items-center gap-x-2 hover:duration-300 hover:scale-110 hover:transition-all'>
-            <Link className='uppercase text-white font-bold text-2xl tracking-widest' href={ROUTES.CART}>
-              Carrito
-            </Link>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-6 invert transform scale-125">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-            </svg>
-          </div>
-        </div> */}
+        <Image
+          height={50}
+          src={logo}
+          className='hidden md:block'
+          alt='logo'
+        />
 
         {/* Botón hamburguesa*/}
         <button 
@@ -105,21 +93,21 @@ export default function Navbar() {
         </div>
 
         {/* Contenido del menú móvil */}
-        <div className="flex flex-col items-center justify-center h-full space-y-12 pb-16">
+        <div className="flex flex-col items-center justify-center h-full space-y-12 pb-32">
 
           {/* Item Cuenta */}
           <div 
-            className={`flex flex-col items-center space-y-4 transform transition-all duration-700 ease-out ${
+            className={`group flex flex-col items-center space-y-4 transform transition-all duration-700 ease-out ${
               isMenuOpen ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0'
             }`}
             style={{ transitionDelay: isMenuOpen ? '200ms' : '0ms' }}
           >
             <div className='flex items-center justify-center space-x-3'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white animate-bounce-alt">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white group-hover:text-purple-500 group-hover:duration-300 animate-bounce-alt">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
               </svg>
               <Link 
-                className='uppercase text-white font-bold text-3xl tracking-widest hover:text-gray-300 transition-colors duration-300' 
+                className='uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
                 href={ROUTES.ADMIN}
                 onClick={closeMenu}
               >
@@ -130,17 +118,17 @@ export default function Navbar() {
 
           {/* Item Productos */}
           <div 
-            className={`flex flex-col items-center space-y-4 transform transition-all duration-700 ease-out ${
+            className={`group flex flex-col items-center space-y-4 transform transition-all duration-700 ease-out ${
               isMenuOpen ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0'
             }`}
             style={{ transitionDelay: isMenuOpen ? '200ms' : '0ms' }}
           >
             <div className='flex items-center justify-center space-x-3'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white animate-bounce-alt">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white group-hover:text-purple-500 group-hover:duration-300 animate-bounce-alt">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
               </svg>
               <Link 
-                className='uppercase text-white font-bold text-3xl tracking-widest hover:text-gray-300 transition-colors duration-300' 
+                className='uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
                 href={ROUTES.HOME}
                 onClick={closeMenu}
               >
@@ -149,19 +137,41 @@ export default function Navbar() {
             </div>
           </div>
 
-          {/* Item Carrito */}
+          {/* Item Favoritos */}
           <div 
-            className={`flex flex-col items-center space-y-4 transform transition-all duration-700 ease-out ${
+            className={`group flex flex-col items-center space-y-4 transform transition-all duration-700 ease-out ${
               isMenuOpen ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0'
             }`}
             style={{ transitionDelay: isMenuOpen ? '400ms' : '0ms' }}
           >
             <div className='flex items-center justify-center space-x-3'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white animate-bounce-alt">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white group-hover:text-purple-500 group-hover:duration-300 animate-bounce-alt">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+              </svg>
+
+              <Link 
+                className='uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
+                href={ROUTES.HOME}
+                onClick={closeMenu}
+              >
+                Favoritos
+              </Link>
+            </div>
+          </div>
+
+          {/* Item Carrito */}
+          <div 
+            className={`group flex flex-col items-center space-y-4 transform transition-all duration-700 ease-out ${
+              isMenuOpen ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0'
+            }`}
+            style={{ transitionDelay: isMenuOpen ? '400ms' : '0ms' }}
+          >
+            <div className='flex items-center justify-center space-x-3'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white group-hover:text-purple-500 group-hover:duration-300 animate-bounce-alt">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
               </svg>
               <Link 
-                className='uppercase text-white font-bold text-3xl tracking-widest hover:text-gray-300 transition-colors duration-300' 
+                className='uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
                 href={ROUTES.CART}
                 onClick={closeMenu}
               >
