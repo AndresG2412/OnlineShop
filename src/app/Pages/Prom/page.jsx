32afd16165f32800bc1ffaf6c5f37ba40ad841d9
Andres Gaviria <cgaviria930@gmail.com>
2025-06-04 20:11:35 -0500

    pagina promociones

--- src/app/Pages/Prom/page.jsx
+++ src/app/Pages/Prom/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import sadbag from '../../images/notfound.webp'
+import Image from 'next/image'
 
 export default function Promociones() {
-  return (
-    <div>
-      Promociones Page
-    </div>
-  )
+    return (
+        <div className='w-full h-screen pt-16 flex items-center justify-center'>
+            <Image alt='sadbag_image' className='w-[55%] h-96' src={sadbag}/>
+        </div>
+    )
 }
