60093ae40a16b4c98cb84db26aea6c899b169789
Andres Gaviria <cgaviria930@gmail.com>
2025-06-16 12:12:27 -0500

    añadido botones

--- src/app/Pages/[productId]/page.jsx
+++ src/app/Pages/[productId]/page.jsx
@@ -64,7 +64,7 @@ export default function Producto() {
                 <div className="w-1/2 flex flex-col items-start gap-y-12 justify-center">
                     <h1 className="text-5xl font-bold mb-4">{producto.titulo}</h1>
                     <p className="text-2xl font-semibold">Precio: ${producto.precio}</p>
-                    <div>
+                    <div className="">
                         <p className="text-[1.775rem]">Tallas Disponibles:</p>
                         <div className="mt-4 flex gap-x-5 items-center">
                             <div className={`w-10 h-10 flex items-center justify-center rounded-sm font-semibold border-dashed border-3 border-black ${producto.tallaS ? "bg-green-200" : "bg-red-200"}`}>S</div>
@@ -76,7 +76,29 @@ export default function Producto() {
                     {/* poner carrito, like, unidades disponibles aun, tiempo limitado, por temporada, etc.
 
                     poner vista el celular */}
+
                     </div>
+
+                    <div className="w-full flex gap-x-4 items-center justify-between">
+                        <button className="group w-1/2 border-2 border-black py-2 font-semibold rounded-md bg-blue-400 hover:bg-blue-700">
+                            <p className="flex gap-x-1 justify-center items-center group-hover:text-white group-hover:scale-125 duration-300">
+                                Añadir a Carrito
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                                </svg>
+                            </p>
+                        </button>
+                        <button className="group w-1/2 border-2 border-black py-2 font-semibold rounded-md bg-red-400 hover:bg-red-700">
+                            <p className="flex gap-x-1 justify-center items-center group-hover:text-white group-hover:scale-125 duration-300">
+                                Añadir a Me Gusta
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                </svg>
+                            </p>
+                        </button>
+                    </div>
+
+                    <a className="text-red-600 hover:cursor-pointer hover:scale-110 duration-300 font-semibold text-lg">¿ Tienes Preguntas ? Contactanos!</a>
                 </div>
             </div>
 
