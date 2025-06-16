5b885d4e11c62f290c110e73ed21157b1ede3a3a
Andres Gaviria <cgaviria930@gmail.com>
2025-06-15 20:57:00 -0500

    vista pc de  producto

--- src/app/Pages/[productId]/page.jsx
+++ src/app/Pages/[productId]/page.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { db } from "../../../Libs/firebase";
 import { doc, getDoc, collection, getDocs } from "firebase/firestore";
+import Image from "next/image";
 
 export default function Producto() {
     const { productId } = useParams();
@@ -51,21 +52,27 @@ export default function Producto() {
     return (
         <div className="text-black  w-screen flex items-center justify-center mt-24">
             
-            <div className="bg-white p-6 rounded-xs w-2/3">
-                <div>
+            <div className="bg-white flex p-6 rounded-xs w-2/3">
+                <Image
+                    src={`https://res.cloudinary.com/duwosb0hu/image/upload/v1745037571/rata1_bblpf1.png`}
+                    alt={producto.titulo}
+                    width={500}
+                    height={500}
+                    className="w-1/2 h-auto rounded-md"
+                />
 
-                </div>
-
-                <div>
-                    <h1 className="text-3xl font-bold mb-4">{producto.titulo}</h1>
-                    <p className="text-xl">Precio: ${producto.precio}</p>
-                    <p>Tallas Disponibles:</p>
-                        <div className="mt-2 flex gap-x-3 items-center">
-                            <div className={`w-8 h-8 flex items-center justify-center rounded-sm font-semibold border-dashed border-3 border-black ${producto.tallaS ? "bg-green-200" : "bg-red-200"}`}>S</div>
-                            <div className={`w-8 h-8 flex items-center justify-center rounded-sm font-semibold border-dashed border-3 border-black ${producto.tallaM ? "bg-green-200" : "bg-red-200"}`}>M</div>
-                            <div className={`w-8 h-8 flex items-center justify-center rounded-sm font-semibold border-dashed border-3 border-black ${producto.tallaL ? "bg-green-200" : "bg-red-200"}`}>L</div>
-                            <div className={`w-8 h-8 flex items-center justify-center rounded-sm font-semibold border-dashed border-3 border-black ${producto.tallaXL ? "bg-green-200" : "bg-red-200"}`}>XL</div>
+                <div className="w-1/2 flex flex-col items-start gap-y-12 justify-center">
+                    <h1 className="text-5xl font-bold mb-4">{producto.titulo}</h1>
+                    <p className="text-2xl font-semibold">Precio: ${producto.precio}</p>
+                    <div>
+                        <p className="text-[1.775rem]">Tallas Disponibles:</p>
+                        <div className="mt-4 flex gap-x-5 items-center">
+                            <div className={`w-10 h-10 flex items-center justify-center rounded-sm font-semibold border-dashed border-3 border-black ${producto.tallaS ? "bg-green-200" : "bg-red-200"}`}>S</div>
+                            <div className={`w-10 h-10 flex items-center justify-center rounded-sm font-semibold border-dashed border-3 border-black ${producto.tallaM ? "bg-green-200" : "bg-red-200"}`}>M</div>
+                            <div className={`w-10 h-10 flex items-center justify-center rounded-sm font-semibold border-dashed border-3 border-black ${producto.tallaL ? "bg-green-200" : "bg-red-200"}`}>L</div>
+                            <div className={`w-10 h-10 flex items-center justify-center rounded-sm font-semibold border-dashed border-3 border-black ${producto.tallaXL ? "bg-green-200" : "bg-red-200"}`}>XL</div>
                         </div>
+                    </div>
                 </div>
             </div>
 
