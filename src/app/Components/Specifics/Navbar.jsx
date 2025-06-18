25202cfd06720ba5a6723edcf45b400a8ecf6148
Andres Gaviria <cgaviria930@gmail.com>
2025-06-18 17:57:48 -0500

    aviso de paginas en desarrollo

--- src/app/Components/Specifics/Navbar.jsx
+++ src/app/Components/Specifics/Navbar.jsx
@@ -6,6 +6,8 @@ import Image from 'next/image'
 import logo from '../../images/LogoCompleto.png'
 import LogoCorto from "../../images/LogoCorto.png"
 
+import toast from 'react-hot-toast'
+
 import { ROUTES } from '@/routes';
 
 export default function Navbar() {
@@ -18,6 +20,23 @@ export default function Navbar() {
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
+
+  const aviso = () => {
+    toast.error('Esta sección está en desarrollo, pronto estará disponible.', {
+      duration: 3000,
+      position: 'top-center',
+      style: {
+        background: '#111',
+        color: '#fff',
+        fontSize: '16px',
+        fontWeight: 'bold',
+      },
+      iconTheme: {
+        primary: '#ff0000',
+        secondary: '#fff',
+      },
+    });
+  }
 
   return (
     <>
@@ -108,13 +127,19 @@ export default function Navbar() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white group-hover:text-purple-500 group-hover:duration-300 animate-bounce-alt">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
               </svg>
-              <Link 
+              {/* <Link 
                 className='uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
                 href={ROUTES.FORMS}
-                onClick={closeMenu}
+                onClick={aviso}
               >
                 Cuenta
-              </Link>
+              </Link> */}
+              <p 
+                className='hover:cursor-pointer uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
+                onClick={aviso}
+              >
+                Cuenta
+              </p>
             </div>
           </div>
 
@@ -150,14 +175,19 @@ export default function Navbar() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white group-hover:text-purple-500 group-hover:duration-300 animate-bounce-alt">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
               </svg>
-
-              <Link 
+              {/* <Link 
                 className='uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
                 href={ROUTES.LIKE}
-                onClick={closeMenu}
+                onClick={aviso}
               >
                 Favoritos
-              </Link>
+              </Link> */}
+              <p 
+                className='hover:cursor-pointer uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
+                onClick={aviso}
+              >
+                Favoritos
+              </p>
             </div>
           </div>
 
@@ -172,13 +202,19 @@ export default function Navbar() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="scale-125 text-white group-hover:text-purple-500 group-hover:duration-300 animate-bounce-alt">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
               </svg>
-              <Link 
+              {/* <Link 
                 className='uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
                 href={ROUTES.CAR}
-                onClick={closeMenu}
+                onClick={aviso}
               >
                 Carrito
-              </Link>
+              </Link> */}
+              <p 
+                className='hover:cursor-pointer uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
+                onClick={aviso}
+              >
+                Carrito
+              </p>
             </div>
           </div>
 
@@ -194,14 +230,19 @@ export default function Navbar() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6Z" />
               </svg>
-
-              <Link 
+              {/* <Link 
                 className='uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
                 href={ROUTES.PROM}
-                onClick={closeMenu}
+                onClick={aviso}
               >
                 Ofertas
-              </Link>
+              </Link> */}
+              <p 
+                className='hover:cursor-pointer uppercase text-white font-bold text-3xl tracking-widest group-hover:text-purple-500 group-hover:duration-300 transition-colors duration-300' 
+                onClick={aviso}
+              >
+                Ofertas
+              </p>
             </div>
           </div>
         </div>
