538d4c9b4fb96f033bc3ab4d42799ceb976e200c
Andres Gaviria <cgaviria930@gmail.com>
2025-06-20 19:39:04 -0500

    arreglo errores

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,9 +5,9 @@ import { db } from "../Libs/firebase"
 import { doc, getDoc, collection, getDocs } from "firebase/firestore";
 import Link from "next/link";
 
-import Card from "./components/Card"; // tarjeta de producto, recibe parametros de "titulo", "precio" e "imageLink"
-import Section from "./components/Sections"; // titulo de la seccion de un producto especifico, recibe parametro de "nombre"
-import Assets from "./components/Specifics/Assets";
+import Card from "../app/components/Card"; // tarjeta de producto, recibe parametros de "titulo", "precio" e "imageLink"
+import Section from "../app/components/Sections"; // titulo de la seccion de un producto especifico, recibe parametro de "nombre"
+import Assets from "../app/components/Specifics/Assets";
 
 export default function Main() {
   const [secciones, setSecciones] = useState([]);
